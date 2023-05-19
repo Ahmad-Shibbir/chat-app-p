b07185394760988bd0eff555cfcbfd8e8e132816
Ahmad Shibbir <ahmadshibbir.iium@gmail.com>
2023-05-19 12:24:24 +0800

    error handler added for html and json response

--- middlewares/common/errorHandler.js
+++ middlewares/common/errorHandler.js
@@ -8,7 +8,20 @@ function notFoundHandler(req,res, next){
 
 // function defultl
 function errorHandler(err, req, res, next){
-    
+    res.locals.error=
+        process.env.NODE_ENV === 'development' ? err: {message: err.message};
+
+    res.status(err.status || 500);
+
+    if(res.locals.html){
+        // html response
+        res.render('error',{
+            title:"Error page",
+        });
+    }else{
+        // json response
+        res.json(res.locals.error)
+    }
 }
 module.exports={
     notFoundHandler,
