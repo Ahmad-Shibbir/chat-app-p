014c4e27306841324efae4fec29f36ca6ea41118
Ahmad Shibbir <ahmadshibbir.iium@gmail.com>
2023-05-20 00:50:00 +0800

    multer added for file upload

--- router/userRouter.js
+++ router/userRouter.js
@@ -4,10 +4,13 @@ const express = require("express");
 // internal import
 const {getUsers}=require("../controller/usersController")
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const avatarUpload=require("../middlewares/common/users/avatarUpload")
 
 const router = express.Router();
 
 // Users page
 router.get("/", decorateHtmlResponse("Users"),getUsers);
+
+router.post("/", avatarUpload)
 
 module.exports = router
